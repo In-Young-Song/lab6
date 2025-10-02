c08d9dbf0d286df5d3a124cba598090d53135518
In Young Song <231744384+In-Young-Song@users.noreply.github.com>
2025-10-02 21:48:42 +0000

    Fix test_sample_run_anonymizer call and asserts

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,11 +2,7 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 
 
 def test_sample_run_anonymizer():
-    text = "My name is Bond."
-    start = 11
-    end = 15
-
-    result = sample_run_anonymizer(text, start, end)
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
 
     # Check anonymized text
     assert result.text == "My name is BIP."
@@ -14,7 +10,10 @@ def test_sample_run_anonymizer():
     # Check there is exactly one replacement item
     assert len(result.items) == 1
 
+    # Convert OperatorResult object to dict
     item = result.items[0].to_dict()
+
+    # Check start and end positions
     assert item["start"] == 11
     assert item["end"] == 14
     assert item["entity_type"] == "PERSON"
